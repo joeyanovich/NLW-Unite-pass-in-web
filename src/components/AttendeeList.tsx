be975ab1b6637896100b356489add4c582060c0d
joeyanovich <joedison.dias@gmail.com>
2024-04-05 23:53:28 -0300

    feat ✨: create and style buttons

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -1,4 +1,4 @@
-import { Search } from "lucide-react"
+import { Search, MoreHorizontal, ChevronsLeft, ChevronLeft, ChevronRight, ChevronsRight } from "lucide-react"
 
 export function AttendeeList() {
   return (
@@ -14,14 +14,14 @@ export function AttendeeList() {
         <table className="w-full">
           <thead>
             <tr className="border-b border-white/10">
-              <th className="py-3 px-4 text-sm font-semibold text-left">
+              <th style={{ width: 48 }} className="py-3 px-4 text-sm font-semibold text-left">
                 <input type="checkbox" name="" id="" />
               </th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Código</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Participantes</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Data de inscrição</th>
               <th className="py-3 px-4 text-sm font-semibold text-left">Data do check-in</th>
-              <th className="py-3 px-4 text-sm font-semibold text-left"></th>
+              <th style={{ width: 64 }} className="py-3 px-4 text-sm font-semibold text-left"></th>
             </tr>
           </thead>
           <tbody>
@@ -40,7 +40,11 @@ export function AttendeeList() {
                 </td>
                 <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
                 <td className="py-3 px-4 text-sm text-zinc-300">3 dias atrás</td>
-                <td className="py-3 px-4 text-sm text-zinc-300"></td>
+                <td className="py-3 px-4 text-sm text-zinc-300">
+                  <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
+                    <MoreHorizontal className="size-4"/>
+                  </button>
+                </td>
               </tr>
               )
             })}
@@ -51,7 +55,23 @@ export function AttendeeList() {
                 Mostrando 10 de 228 itens
               </td>
               <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
-                Página 1 de 23
+                <div className="inline-flex gap-8 items-center">
+                  <span>Página 1 de 23</span>
+                  <div className="flex gap-1.5">
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronsLeft className="size-4"/>
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronLeft className="size-4"/>
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronRight className="size-4"/>
+                    </button>
+                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                      <ChevronsRight className="size-4"/>
+                    </button>
+                  </div>
+                </div>
               </td>
             </tr>
           </tfoot>
