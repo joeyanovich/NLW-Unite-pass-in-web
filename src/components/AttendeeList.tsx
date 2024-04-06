dc0ec1e752fdeaf5e3d667afb365fde2ba4b9f06
joeyanovich <joedison.dias@gmail.com>
2024-04-05 23:10:25 -0300

    add ➕: install lucide lib

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -1,7 +1,11 @@
 export function AttendeeList() {
   return (
-    <>
-      Attendee List
-    </>
+    <div className="flex gap-3 items-center">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
+        icone
+        <input className="bg-transparent flex-1 outline-none" type="text" placeholder="Buscar participantes..."/>
+      </div>
+    </div>
   )
 }
