b5915c15d300768e273624f20a70f07732271b17
joeyanovich <joedison.dias@gmail.com>
2024-04-10 22:48:15 -0300

    feat ✨: page filter saved in url

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -23,7 +23,15 @@ interface Attendee {
 
 export function AttendeeList() {
   const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if(url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
 
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
@@ -48,22 +56,32 @@ export function AttendeeList() {
       })
   }, [page, search])
 
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set('page', String(page))
+
+    window.history.pushState({}, "", url)
+
+    setPage(page)
+  }
+
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value)
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   return (
