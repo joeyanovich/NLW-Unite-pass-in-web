04b719b473f4400e34a91141eb6b3f3f4b7ae4c5
joeyanovich <joedison.dias@gmail.com>
2024-04-10 22:26:49 -0300

    style 💄: remove border when focus in search input

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -72,7 +72,12 @@ export function AttendeeList() {
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg  flex items-center gap-3">
           <Search className="size-4 text-emerald-300"/>
-          <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" type="text" placeholder="Buscar participantes..."/>
+          <input
+            onChange={onSearchInputChanged}
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+            type="text"
+            placeholder="Buscar participantes..."
+          />
         </div>
       </div>
       <Table>
